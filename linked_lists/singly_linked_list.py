e67db72c09fd3b213773f8d4353427406447e407
Chirag <26590501+chirag-b@users.noreply.github.com>
2021-12-20 06:00:14 +0000

    First draft of SLL

--- linked_lists/singly_linked_list.py
+++ linked_lists/singly_linked_list.py
@@ -28,6 +28,7 @@ class Node:
         self.value = value # This can be of any type int, string, float...
         self.next = next  # In other languages this will be of type 'Node'.
 
+
 class SinglyLinkedList:
     def __init__(self):
         # Returns a Singly Linked List instance
@@ -35,9 +36,11 @@ class SinglyLinkedList:
         self.tail = None
         self.size = 0
         
+
     def get_size(self):
         # Returns the size of the list
         return self.size
+
 
     def is_empty(self):
         # Returns True if list is empty else returns False
@@ -60,18 +63,78 @@ class SinglyLinkedList:
             self.size += 1
 
 
-    def add_node_at(self, value, position):
+    def add_node_at(self, position, value):
         # Adds a node at 'position'
-        pass
+        if not (position <= self.size):
+            logger.info('Invalid position specified!!') 
+        else:
+            if self.is_empty():
+                logger.info('List empty adding node at head.')
+                node = Node(value)
+                self.head = node
+                self.tail = node
+                self.size += 1
+            elif position == 1:
+                self.add_first(value)
+            else:
+                trav = self.head
+                for _ in range(position-2):
+                    trav = trav.next
+                node = Node(value, trav.next)
+                trav.next = node
+                self.size += 1
+
 
     def remove_node(self, value):
         # Removes a node with 'value'
-        pass
+        if self.is_empty():
+            logger.info('Nothing to remove, the list is empty.')
+        else:
+            trav = self.head
+            trav2 = self.head.next
+            position = self.get_index_of(value)
+            for _ in range(position-2):
+                trav = trav.next
+                trav2 = trav2.next
+            trav.next = trav2.next
+            trav2.next = None
+            self.size -= 1
+
+
+    def get_index_of(self, value):
+        # Returns the index of the value in the list
+        if self.is_empty():
+            logger.info('The list is empty!!')
+        else:
+            trav = self.head
+            for position in range(self.size):
+                if (trav.next != None) and (trav.value != value):
+                    trav = trav.next
+                else:
+                    break
+            position += 1
+            return position
+
 
     def remove_node_at(self, position):
         # Removes node at 'position'
-        pass
-    
+        if not (position <= self.size):
+            logger.info('Invalid position specified!!') 
+        else:
+            if self.is_empty():
+                logger.info('The list is empty!!')
+            else:
+                assert(position <= self.size)
+                trav = self.head
+                trav2 = self.head.next
+                for _ in range(position-2):
+                    trav = trav.next
+                    trav2 = trav2.next
+                trav.next = trav2.next
+                trav2.next = None
+                self.size -= 1
+
+
     def list_all_nodes(self):
         # Lists all nodes currently in the Singly Linked List
         if self.is_empty():
@@ -85,40 +148,114 @@ class SinglyLinkedList:
                 trav = trav.next
             logger.info(all_nodes)
 
+
     def get_value_at(self, position):
         # Returns the value of a node at 'position'
-        pass
+        if not (position <= self.size):
+            logger.info('Invalid position specified!!') 
+        else:
+            if self.is_empty():
+                logger.info('The list is empty!!')
+            else:
+                assert(position <= self.size)
+                trav = self.head
+                for _ in range(position - 1):
+                    trav = trav.next
+                return trav.value
+
 
     def add_first(self, value):
         # Adds a new node to the head of the list
-        pass
+        if self.is_empty():
+            logger.info('List empty adding node at head.')
+            node = Node(value)
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            node = Node(value, self.head)
+            self.head = node
+            self.size += 1
+
 
     def remove_first(self):
         # Removes the node at the head
-        pass
+        if self.is_empty():
+            logger.info('The list is empty!!')
+        else:
+            trav = self.head
+            self.head = self.head.next
+            trav.next = None
+            self.size -= 1
+            del trav    
+
 
     def remove_last(self):
         # Removes the last node in the list
-        pass
+        if self.is_empty():
+            logger.info('The list is empty!!')
+        else:
+            trav = self.head
+            for _ in range(self.size - 2):
+                trav = trav.next
+            trav.next = None
+            self.tail = trav
+            self.size -= 1
+
 
     def get_head_value(self):
         return self.head.value
 
+
     def get_tail_value(self):
         return self.tail.value
+
 
 if __name__ == '__main__':
     # import pdb; pdb.set_trace()
     newSLL = SinglyLinkedList()
-    newSLL.add_node(5)
-    newSLL.add_node(6)
-    newSLL.add_node(7)
-    newSLL.add_node(8)
-    newSLL.add_node(9)
-    newSLL.add_node(10)
-    newSLL.add_node(11)
-    logger.info(newSLL.get_size())
-    logger.info(newSLL.is_empty())
-    logger.info(newSLL.get_head_value())
-    logger.info(newSLL.get_tail_value())
-    newSLL.list_all_nodes()
+    # newSLL.add_node(5)
+    # newSLL.add_node(6)
+    # newSLL.add_node(7)
+    # newSLL.add_node(8)
+    # newSLL.add_node(9)
+    # newSLL.add_node(10)
+    # newSLL.add_node(11)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.add_node_at(6, 66)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.add_node_at(2, 555)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.add_node_at(1, 4)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.add_node_at(1, 3)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.remove_node(6)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.remove_node(555)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.remove_node_at(7)
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.remove_first()
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
+
+    # newSLL.remove_last()
+    # logger.info(newSLL.get_size())
+    # newSLL.list_all_nodes()
